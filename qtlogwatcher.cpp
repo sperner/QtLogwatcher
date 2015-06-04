a7a85d59ccd0e04e06d4f3233beb89ec73de69d1
cethss <cethss@gmail.com>
2015-06-04 23:17:47 +0200

    Fixed false remote address

--- qtlogwatcher.cpp
+++ qtlogwatcher.cpp
@@ -180,6 +180,7 @@ void qtlogwatcher::connectToServer( )
                 connect( tcpSocket, SIGNAL(readyRead()), this, SLOT(doClientReceive()) );
                 tcpSocket->connectToHost( settingsDialog->twHosts->item( i, 0 )->text(),
                                           settingsDialog->twHosts->item( i, 1 )->text().toInt() );
+                tcpSocket->setPeerName( settingsDialog->twHosts->item( i, 0 )->text() );
             }
             else if( settingsDialog->twHosts->item( i, 2 )->text().compare( "udp" ) == 0 )
             {
@@ -190,6 +191,7 @@ void qtlogwatcher::connectToServer( )
                 connect( udpSocket, SIGNAL(readyRead()), this, SLOT(doClientReceive()) );
                 udpSocket->connectToHost( settingsDialog->twHosts->item( i, 0 )->text(),
                                           settingsDialog->twHosts->item( i, 1 )->text().toInt() );
+                udpSocket->setPeerName( settingsDialog->twHosts->item( i, 0 )->text() );
             }
             else if( settingsDialog->twHosts->item( i, 2 )->text().compare( "ssl" ) == 0 )
             {
@@ -200,6 +202,7 @@ void qtlogwatcher::connectToServer( )
                 connect( sslSocket, SIGNAL(readyRead()), this, SLOT(doClientReceive()) );
                 sslSocket->connectToHostEncrypted( settingsDialog->twHosts->item( i, 0 )->text(),
                                           settingsDialog->twHosts->item( i, 1 )->text().toInt() );
+                sslSocket->setPeerName( settingsDialog->twHosts->item( i, 0 )->text() );
             }
         }
     }
